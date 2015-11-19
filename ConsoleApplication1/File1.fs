fb8d3071c64d2006275ecae90a84a09786467e8c
William Narmontas <william@scalawilliam.com>
2015-11-19 06:18:06 +0000

    Initial commit

---
+++ ConsoleApplication1/File1.fs
@@ -0,0 +1,16 @@
+﻿module File1
+
+open Microsoft.FSharp.Data.TypeProviders
+open FSharp.Data
+open FSharp.Data.JsonExtensions
+type Duel = JsonProvider<"""http://api.duel.gg/duels/recent/""", SampleIsList = true>
+
+let doit = 
+    let lv = JsonValue.Load("""http://api.duel.gg/duels/recent/""")
+    match lv with 
+    | JsonValue.Array a ->
+        for record in a do
+            let q = Duel.Parse(record.ToString())
+            for player in q.Players do
+                printfn "Player found: %s" player.Name
+    | _ -> printfn "Failed"
---
+++ ConsoleApplication1/Program.fs
@@ -0,0 +1,23 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+// Use the OData type provider to create types that can be used to access the Northwind database.
+// Add References to FSharp.Data.TypeProviders and System.Data.Services.Client
+open System
+open EventSource4Net
+open System.Threading
+[<EntryPoint>]
+let main argv = 
+    let uri = new Uri("http://api.duel.gg/games/live/")
+    let es = new EventSource(uri, 5000)
+
+    es.StateChanged.Add(fun e -> Console.WriteLine("New state: " + e.State.ToString()))
+    es.EventReceived.Add(fun e -> Console.WriteLine("--------- Msg received -----------\n" + e.Message.ToString()))
+    let ct = new CancellationToken()
+    es.Start(ct) |> ignore
+    printfn "Launching"
+    File1.doit |> ignore
+    printfn "Done it"
+    printfn "%A" argv
+    Console.ReadLine() |> ignore
+    0 // return an integer exit code
